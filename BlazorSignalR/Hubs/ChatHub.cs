31cccbe6a8fa2a441dc1309ab7e276de6f2bb249
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2023-12-22 14:56:48 +0630

    Add project files.

---
+++ BlazorSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task ServerReceiveMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ClientReceiveMessage", user + $" => {Context.ConnectionId}", message); // events
+        }
+
+        public async Task KickServerReceiveMessage(string connectionId)
+        {
+            await Clients.Client(connectionId).SendAsync("KickClientReceiveMessage"); // events
+        }
+    }
+}
---
+++ BlazorSignalR/Pages/Index.razor.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace BlazorSignalR.Pages
+{
+    public partial class Index
+    {
+        private HubConnection? hubConnection;
+        private List<string> messages = new List<string>();
+        private string? userInput;
+        private string? messageInput;
+        private string? connectionId;
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl(Navigation.ToAbsoluteUri("/chathub"))
+                .Build();
+
+            hubConnection.On<string, string>("ClientReceiveMessage", (user, message) =>
+            {
+                var encodedMsg = $"{user}: {message}";
+                messages.Add(encodedMsg);
+                InvokeAsync(StateHasChanged);
+            });
+
+            hubConnection.On("KickClientReceiveMessage", () =>
+            {
+                Navigation.NavigateTo("/counter");
+            });
+
+            await hubConnection.StartAsync();
+        }
+
+        private async Task Send()
+        {
+            if (hubConnection is not null)
+            {
+                await hubConnection.SendAsync("ServerReceiveMessage", userInput, messageInput);
+            }
+        }
+
+        private async Task Kick()
+        {
+            if (hubConnection is not null)
+            {
+                await hubConnection.SendAsync("KickServerReceiveMessage", connectionId);
+            }
+        }
+
+        public bool IsConnected =>
+            hubConnection?.State == HubConnectionState.Connected;
+
+        public async ValueTask DisposeAsync()
+        {
+            if (hubConnection is not null)
+            {
+                await hubConnection.DisposeAsync();
+            }
+        }
+    }
+}
